80f39af413d05b6dc6207a5b0ee73775ff3bd8ae
alex-toy <alexei.80@hotmail.fr>
2024-03-02 08:02:46 +0100

    init

---
+++ AsyncAwaitApp/DecodeBytes/Program.cs
@@ -0,0 +1,15 @@
+﻿#region Sync version
+void Calculate(decimal a, decimal b)
+{
+    Console.WriteLine($"Operation started in thread {Environment.CurrentManagedThreadId}");
+    Console.WriteLine($"Result : {Sum(a, b)}");
+    Console.WriteLine($"Operation finished in thread {Environment.CurrentManagedThreadId}");
+}
+
+decimal Sum(decimal a, decimal b)
+{
+    Console.WriteLine($"Sum running in thread {Environment.CurrentManagedThreadId}");
+    return a + b;
+}
+#endregion
+
