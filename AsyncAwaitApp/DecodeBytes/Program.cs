3013bd229d9a6ee33bc84a6f6f62c3919d5b676a
alex-toy <alexei.80@hotmail.fr>
2024-03-02 08:42:05 +0100

    async await

--- AsyncAwaitApp/DecodeBytes/Program.cs
+++ AsyncAwaitApp/DecodeBytes/Program.cs
@@ -1,15 +1,55 @@
 ﻿#region Sync version
+using System.Runtime.CompilerServices;
+
 void Calculate(decimal a, decimal b)
 {
     Console.WriteLine($"Operation started in thread {Environment.CurrentManagedThreadId}");
     Console.WriteLine($"Result : {Sum(a, b)}");
     Console.WriteLine($"Operation finished in thread {Environment.CurrentManagedThreadId}");
 }
+#endregion
+
+#region V2
+void CalculateV2(decimal a, decimal b)
+{
+    Console.WriteLine($"Operation started in thread {Environment.CurrentManagedThreadId}");
+    Task task = Task.Factory.StartNew(() =>
+    {
+        Sum(a, b);
+    });
+
+    TaskAwaiter awaiter = task.GetAwaiter();
+    awaiter.OnCompleted(() =>
+    {
+        Console.WriteLine($"Operation finished in thread {Environment.CurrentManagedThreadId}");
+    });
+
+    Console.WriteLine($"Operation continues in thread {Environment.CurrentManagedThreadId}");
+}
+#endregion
+
+#region V3
+async Task CalculateV3(decimal a, decimal b)
+{
+    Console.WriteLine($"Operation started in thread {Environment.CurrentManagedThreadId}");
+    Console.WriteLine($"Result : {await SumAsync(a, b)}");
+    Console.WriteLine($"Operation finished in thread {Environment.CurrentManagedThreadId}");
+}
+
+async Task<decimal> SumAsync(decimal a, decimal b)
+{
+    await Task.Delay(TimeSpan.FromMilliseconds(1000));
+    Console.WriteLine($"Sum running in thread {Environment.CurrentManagedThreadId}");
+    return a + b;
+}
+#endregion
 
 decimal Sum(decimal a, decimal b)
 {
     Console.WriteLine($"Sum running in thread {Environment.CurrentManagedThreadId}");
     return a + b;
 }
-#endregion
 
+//Calculate(2, 3);
+//CalculateV2(2, 3);
+await CalculateV3(2, 3);
---
+++ AsyncAwaitApp/DiscoveryAsync/Program.cs
@@ -0,0 +1,14 @@
+﻿async Task Execute(int duration = 3)
+{
+    Console.WriteLine("start Execute");
+    await Task.Delay(TimeSpan.FromSeconds(duration));
+    Console.WriteLine("end Execute");
+}
+
+Console.WriteLine("start of program");
+//Execute();
+//await Execute();
+Task task = Execute();
+Console.WriteLine("end of program");
+await task;
+Console.ReadLine();
